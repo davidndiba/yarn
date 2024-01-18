eb11c16eda3e31d8157d97b8069097f22c5f2650
David <david.ndiba@student.moringaschool.com>
2024-01-18 22:41:47 +0300

    menu and navbar

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,30 +1,28 @@
 
 import React from 'react';
 import styles from './index.less';
+import { Menu } from 'antd';
 
 const BasicLayout: React.FC = (props) => {
   return (
     <div className={styles.container}>
-      {/* Sidebar */}
       <div className={styles.sidebar}>
-        {/* Sidebar Content Here */}
+        <Menu className={styles.menu}>
+          <Menu.Item key="1">Dashboard</Menu.Item>
+          <Menu.Item key="2">Servers</Menu.Item>
+          <Menu.Item key="3">Exchange Sessions</Menu.Item>
+          <Menu.Item key="4">Mapping</Menu.Item>
+        </Menu>
       </div>
 
       {/* Page Content */}
       <div className={styles.content}>
         {/* Navbar */}
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          {/* Navbar Content Here */}
+        <nav className={styles.nav}>
+         Home
+         John Doe
         </nav>
-
-        {/* Main Content */}
         <div className={styles.mainContent}>
-          {/* Search Input */}
-          <div className={styles.searchContainer}>
-            <input type="text" id="searchInput" placeholder="Search..." />
-          </div>
-
-          {/* Cards and Tables */}
           <div className={styles.cardsTablesContainer}>
             {props.children}
           </div>
